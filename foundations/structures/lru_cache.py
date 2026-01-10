e089495c39c250dca198b6b6a9b75e7cd74be813
Rudra Raut <87040167+Rudrarauttt@users.noreply.github.com>
2026-01-10 14:34:48 +0530

    refactor(structures): improve edge-case parameter validation in lru_cache

---
+++ foundations/structures/lru_cache.py
@@ -0,0 +1,76 @@
+
+class Node:
+    def __init__(self, key=None, value=None):
+        self.key, self.value = key, value
+        self.prev = self.next = None
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.head, self.tail = Node(), Node()
+        self.head.next, self.tail.prev = self.tail, self.head
+    def _remove(self, node):
+        node.prev.next, node.next.prev = node.next, node.prev
+    def _add(self, node):
+        node.next, node.prev = self.head.next, self.head
+        self.head.next.prev = self.head.next = node
+    def get(self, key):
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        return None
+    def put(self, key, value):
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self._add(node)
+        self.cache[key] = node
+        if len(self.cache) > self.capacity:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
+
+class Node:
+    def __init__(self, key=None, value=None):
+        self.key, self.value = key, value
+        self.prev = self.next = None
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.head, self.tail = Node(), Node()
+        self.head.next, self.tail.prev = self.tail, self.head
+    def _remove(self, node):
+        node.prev.next, node.next.prev = node.next, node.prev
+    def _add(self, node):
+        node.next, node.prev = self.head.next, self.head
+        self.head.next.prev = self.head.next = node
+    def get(self, key):
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        return None
+    def put(self, key, value):
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self._add(node)
+        self.cache[key] = node
+        if len(self.cache) > self.capacity:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
+# Implementation of lru_cache
+# Last updated: 2026-01-10T14:34:48
+# Complexity: O(log N) or O(1)
+
+def lru_cache_operation(x):
+    '''Execution routine for lru_cache'''
+    return x * 2
+
