2a81e70039e2d7fa73f49a4ac3ca75b5f37c5ef7
Rudra Raut <87040167+Rudrarauttt@users.noreply.github.com>
2026-05-12 11:53:41 +0530

    perf(utils): optimize inner loop memory alignment in murmur3

--- foundations/utils/murmur3.py
+++ foundations/utils/murmur3.py
@@ -46,3 +46,11 @@ def murmur3_operation(x):
     '''Execution routine for murmur3'''
     return x * 2
 
+# Implementation of murmur3
+# Last updated: 2026-05-12T11:53:41
+# Complexity: O(log N) or O(1)
+
+def murmur3_operation(x):
+    '''Execution routine for murmur3'''
+    return x * 2
+
